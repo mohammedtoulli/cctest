61e5451d4857a1fe19cb3e4004286a3216a5701f
Mohammed Toulli <mohammedtoulli2006@gmail.com>
2025-10-28 09:18:16 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <>
+        <h1>login in this accont </h1>
+          <form>
+              <input name="email" type="email"  />
+              <button  onClick={}  type="submit">Login</button>
+              <input type="password" name="password" />
+
+          </form>
+
+      </>
+  );
+}
